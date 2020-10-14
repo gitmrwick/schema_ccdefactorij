cf3c669e7c71eea20e1768d72ad71135b3d5a209
gitmrwick <chadwick.employment@gmail.com>
2020-10-14 15:05:01 +0200

    php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php $events = file_get_contents('http://localhost:44444/wickdingske'); ?>
+<!doctype html>
+
+<html>
+<head>
+  <meta charset="utf8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>schema</title>
+  <style>
+    body {
+      background-color: rgb(20,20,20);
+      color: rgb(200,200,200);
+    }
+  </style>
+</head>
+<body>
+  <script>const events_response=<?=$events?></script>
+  <script src='schema.js'>
+  </script>
+</body>
+</html>
